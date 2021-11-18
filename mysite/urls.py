dc0d6830772c9ebb29999def6720b8d2a2e1c003
Navid Hariri <navihariri25@gmail.com>
2021-11-18 00:24:43 -0800

    slight formatting

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
 
-from chat import views
-
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
